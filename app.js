d6d4e7a5eb93499e7711d6e4e479e3d733363253
ANDRÉ ROBERTO TAVARES <25132696@CFP127ALUNO.LOCAL>
2025-08-12 11:17:54 -0300

    Adicionando o cargo e alterando nomes

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 import funcionarios from './funcionarios.json' with { type: "json" };
 
 function criarImagem(funcionario) {
-    const galeria = document.getElementById('galeria');
+    const Func = document.getElementById('Func');
 
     const container = document.createElement('div');
     const imagem = document.createElement('img');
-    const legenda = document.createElement('span');
+    const nome = document.createElement('p');
+    const cargo = document.createElement('span');
+
 
     imagem.src = "./img/" + funcionario.imagem; 
-    legenda.textContent = funcionario.nome; 
+    nome.textContent = funcionario.nome; 
+    cargo.textContent = funcionario.cargo; 
+
 
     container.appendChild(imagem);
-    container.appendChild(legenda);
-    galeria.appendChild(container);
+    container.appendChild(nome);
+    container.appendChild(cargo);
+    Func.appendChild(container);
 }
 
 function carregarImagens() {
